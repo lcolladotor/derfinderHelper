5c5ff8d651e987628566517a4ba0cdb953191d75
Marc Carlson <mcarlson@fhcrc.org>
2014-10-03 23:09:29 +0000

    Adds GenomicInteractions/ PSEA/ ALDEx2/ polyester/ MSGFplus/ derfinder/ derfinderPlot/ derfinderHelper/ regionReport/ FEM/ MSGFgui/ IdeoViz/ proBAMr/ ASGSCA/ PAA/ paxtoolsr/ flowcatchR/ to the repos.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/derfinderHelper@94975 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/derfinderHelper-package.R
@@ -0,0 +1,13 @@
+#'  derfinder helper package
+#'
+#' Helper package for speeding up the derfinder package when using multiple 
+#' cores. 
+#'
+#' @name derfinderHelper-package
+#' @aliases derfinderHelper-package
+#' @docType package
+#' @author Leonardo Collado-Torres <lcollado@@jhu.edu>
+#'
+#'
+#' @keywords package
+NULL 
