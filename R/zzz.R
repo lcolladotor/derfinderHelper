188984defa5b37b38696df734a16977beaede6e0
Leonardo Collado-Torres <lcollado@jhu.edu>
2015-06-19 20:06:57 +0000

    v0.99.3 -- update citation info, link to regionReport and installation instructions
    From: lcolladotor <lcollado@jhsph.edu>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/derfinderHelper@105220 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -8,7 +8,7 @@
         paste("Find out what's changed in derfinder with\n",
             "news(Version == \"", utils::packageVersion("derfinder"),
             "\", package = \"derfinder\")", sep = ""),
-        "Create HTML reports from derfinder results using derfinderReport available at\nhttps://github.com/lcolladotor/derfinderReport",
+        "Create HTML reports from derfinder results using regionReport available at\nhttp://www.bioconductor.org/packages/devel/bioc/html/regionReport.html",
         "Use suppressPackageStartupMessages to eliminate package startup messages."
     )
     
